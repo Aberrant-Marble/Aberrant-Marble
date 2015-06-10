029c5e692f112f808e3166cbf52b0d480c63085a
Robert Ing <rmi22186@gmail.com>
2015-06-10 16:50:54 -0400

    added postgres test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var crypto = require('crypto');
 var session = require('express-session');
-
+var pg = require('pg');
 // config file to instantiate 
 var User = require('./server/user/userController');
 var Users = require('./db/index');
@@ -30,6 +30,19 @@ app.use(session({
   saveUninitialized: true
 }));
 
+
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+});
 
 app.listen(port);
 
